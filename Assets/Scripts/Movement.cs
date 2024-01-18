dace6bb952f08f71f2f7346b1b71f92ee7943e48
Gabriel Vasquez <gabriel.felipe.008@gmail.com>
2024-01-17 17:56:08 -0700

    add progress until now

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+
+    [SerializeField] float mainThrust = 100f;
+    
+    [SerializeField] float rotationThrust = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+
+        bool spaceIsPressed = Input.GetKey(KeyCode.Space);
+
+        Vector3 thrustVec = Vector3.up * mainThrust * Time.deltaTime;
+
+        if (spaceIsPressed) {
+            rigidBody.AddRelativeForce(thrustVec);
+        }
+        
+    }
+
+    void ProcessRotation() {
+
+        bool dPressed = Input.GetKey(KeyCode.D);
+        bool aPressed = Input.GetKey(KeyCode.A);
+
+        if (aPressed && !dPressed) {
+            ApplyRotation(rotationThrust);
+        }
+        else if (dPressed && !aPressed) {
+            ApplyRotation(rotationThrust * -1);
+        }
+    }
+    
+    void ApplyRotation(float rotationThrustArg) {
+        rigidBody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThrustArg * Time.deltaTime);
+        rigidBody.freezeRotation = false;
+    }
+}
