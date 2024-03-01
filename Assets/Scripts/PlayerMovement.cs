28dfd5c419a2d42f6228333d73099757b3dae7f9
Gabriel Vasquez <gabriel.felipe.008@gmail.com>
2024-03-01 12:19:59 -0700

    add player rig with script (PlayerMovement), master timeline, enemy (sphere), and input actions for player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] InputActionAsset _playerActionsAsset;
+    [SerializeField] float _moveSpeed = 1.0f;
+
+    InputActionMap _movementActionMap;
+    InputAction _moveKeys;
+
+    // Can also manage everything in script or every frame using Update()
+     
+    void Awake() {
+        _movementActionMap = _playerActionsAsset.FindActionMap("movement");
+        _moveKeys = _movementActionMap["move"];
+    }
+
+    void OnEnable() {
+
+        // Enable all actions in this action map
+        _movementActionMap.Enable();
+
+        // _moveKeys.performed += OnMove;
+    }
+
+    void OnDisable() {
+
+        // _moveKeys.performed -= OnMove;
+
+        // Disable all actions in this action map
+        _movementActionMap.Disable();
+    }
+
+    void Update() {
+
+        float horizontalThrow = 0f;
+        float verticalThrow = 0f;
+
+        if (_moveKeys.IsPressed()) {
+            
+            horizontalThrow = _moveKeys.ReadValue<Vector2>().x * Time.deltaTime * _moveSpeed;
+            
+            verticalThrow = _moveKeys.ReadValue<Vector2>().y * Time.deltaTime * _moveSpeed;
+
+        }
+
+        float xPos = horizontalThrow + transform.localPosition.x;
+        float yPos = verticalThrow + transform.localPosition.y;
+        
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+    }
+    
+}
