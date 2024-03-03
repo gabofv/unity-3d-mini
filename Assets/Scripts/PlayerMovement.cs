a509014643870a5af87a60858d9aee7347961ecc
Gabriel Vasquez <gabriel.felipe.008@gmail.com>
2024-03-03 00:22:23 -0700

    implement rotation and tweak serialized vars

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] InputActionAsset _playerActionsAsset;
+
     [SerializeField] float _moveSpeed = 1f;
 
     // The GameObject this is attached to must be at (0;0) for coherence and camara centered
@@ -12,6 +13,16 @@ public class PlayerMovement : MonoBehaviour {
 
     InputActionMap _movementActionMap;
     InputAction _moveAction;
+
+    [SerializeField] float _pitchPositionFactor = 1f;
+    [SerializeField] float _rollPositionFactor = 1f;
+    [SerializeField] float _yawPositionFactor = 1f;
+
+    [SerializeField] float _pitchThrowFactor = 1f;
+    [SerializeField] float _rollThrowFactor = 1f;
+
+    float _xThrow;
+    float _yThrow;
 
     // Can also manage everything in script or every frame using Update()
 
@@ -38,24 +49,37 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update() {
 
-        if (_moveAction.IsPressed()) {
+        if (_moveAction.IsPressed())
+        {
+
             ProcessTranslation();
             ProcessRotation();
+
         }
 
     }
 
     private void ProcessRotation() {
 
+        // Using clamped position values
+        float pitch = -1 * (transform.localPosition.y * _pitchPositionFactor + _yThrow * _pitchThrowFactor);
+        float roll = -1 * (transform.localPosition.x * _rollPositionFactor + _xThrow * _rollThrowFactor);
+        float yaw = transform.localPosition.x * _yawPositionFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
     }
 
     private void ProcessTranslation() {
 
-        float horizontalThrow = _moveAction.ReadValue<Vector2>().x * Time.deltaTime * _moveSpeed;
-        float verticalThrow = _moveAction.ReadValue<Vector2>().y * Time.deltaTime * _moveSpeed;
+        _xThrow = _moveAction.ReadValue<Vector2>().x;
+        _yThrow = _moveAction.ReadValue<Vector2>().y;
+        
+        float xOffset = _xThrow * Time.deltaTime * _moveSpeed;
+        float yOffset = _yThrow * Time.deltaTime * _moveSpeed;
 
-        float rawXPos = horizontalThrow + transform.localPosition.x;
-        float rawYPos = verticalThrow + transform.localPosition.y;
+        float rawXPos = xOffset + transform.localPosition.x;
+        float rawYPos = yOffset + transform.localPosition.y;
 
         float clampedXPos = Mathf.Clamp(rawXPos, -_xAxisRange, _xAxisRange);
         float clampedYPos = Mathf.Clamp(rawYPos, -_yAxisRange, _yAxisRange);
